01eaec12ef8775e1800828a7f1d63c19cabbb022
KevCaz <KevCaz@users.noreply.github.com>
2021-01-15 16:38:49 +0000

    deploy: ee42820fc109d0aca12862400d5b8a6ca14f0d0f

---
+++ README.md
@@ -0,0 +1,2 @@
+# Template for R workshops
+![deploy workshop](https://github.com/inSilecoInc/workshop_R_template/workflows/deploy%20workshop/badge.svg)
---
+++ script.R
@@ -0,0 +1,19 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  warning = FALSE,
+  message = FALSE,
+  dev = "png",
+  dpi = 108,
+  fig.width = 6,
+  fig.height = 4.5,
+  fig.align = 'center',
+  width = 120
+)
+rfa <- function(...) icon::fontawesome(...)
+
+
+## ----plot1--------------------------------------------------------------------
+plot(1,1)
+
